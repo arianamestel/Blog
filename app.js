34db852ab9f06ea6bed5833bfd849d47a47c23c1
arianamestel <arianamestel@yahoo.com>
2021-06-17 23:46:36 -0400

    atlas

--- app.js
+++ app.js
@@ -1,52 +1,55 @@
-var express 			= require("express"),
-	expressSantizer 	= require("express-sanitizer"),
-	methodOverride 		= require("method-override"),
-	app 			= express(),
-	bodyParser 		= require("body-parser"),
-	mongoose 		= require("mongoose"),
-	passport 		= require("passport"),
-	LocalStrategy		= require("passport-local"),
-	flash			= require("connect-flash"),
-	Article 		= require("./models/articles"),
-	User			= require("./models/users"),
-	Comment 		= require("./models/comments"),
-	seedDB			= require("./seeds");
+var express = require("express"),
+  expressSantizer = require("express-sanitizer"),
+  methodOverride = require("method-override"),
+  app = express(),
+  bodyParser = require("body-parser"),
+  mongoose = require("mongoose"),
+  passport = require("passport"),
+  LocalStrategy = require("passport-local"),
+  flash = require("connect-flash"),
+  Article = require("./models/articles"),
+  User = require("./models/users"),
+  Comment = require("./models/comments"),
+  seedDB = require("./seeds");
 
 // requiring routes
-var articleRoutes 	= require("./routes/articles.js"),
-	commentRoutes 	= require("./routes/comments.js"),
-	indexRoutes	= require("./routes/index.js");
+var articleRoutes = require("./routes/articles.js"),
+  commentRoutes = require("./routes/comments.js"),
+  indexRoutes = require("./routes/index.js");
 
 // APP CONFIG
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressSantizer());
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
 //mongoose.connect("mongodb://localhost/TheDebate");
-mongoose.connect("mongodb://ariana:password@ds123790.mlab.com:23790/debate");
-
+mongoose.connect(
+  "mongodb+srv://ariana:password@debate.ekmwo.mongodb.net/debate?retryWrites=true&w=majority"
+);
 
 // seedDB();
 
 //passport config
-app.use(require("express-session")({
-	secret: "Ariana is the best!",
-	resave: false,
-	saveUninitialized: false
-}));
+app.use(
+  require("express-session")({
+    secret: "Ariana is the best!",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(function(req, res, next) {
-	res.locals.currentUser = req.user;
-	res.locals.error = req.flash("error");
-	res.locals.success = req.flash("success");
-	next();
+app.use(function (req, res, next) {
+  res.locals.currentUser = req.user;
+  res.locals.error = req.flash("error");
+  res.locals.success = req.flash("success");
+  next();
 });
 
 app.use("/articles", articleRoutes);
